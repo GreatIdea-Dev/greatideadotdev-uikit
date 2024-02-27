165ac48fc5d753a521dbecb8a858fb3e384f9c05
John Larson <jl@buildmidwestern.com>
2024-02-26 21:10:08 -0600

    init commit

--- app/components/FooterLogoLine/index.tsx
+++ app/components/FooterLogoLine/index.tsx
@@ -1,6 +1,6 @@
 import GreatIdeaBulb from "@/app/Icons/GreatIdeaBulb";
 
-export default function FooterLogoLine({ color }: { color: string }) {
+export default function FooterLogoLine() {
   return (
     <div className="footerLogoLine">
       <div className="footerLogo logoColor">
