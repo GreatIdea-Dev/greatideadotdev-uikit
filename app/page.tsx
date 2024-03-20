95c6812a44dd671af9a4ddf4f6f9cfb5e82f52d1
John Larson <jl@buildmidwestern.com>
2024-03-20 06:37:52 -0500

    updated contact form layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -6,9 +7,31 @@ import { useRouter } from 'next/navigation';
 import CursorSpotlightCard from './components/CursorSpotlight/CursorSpotlightCard';
 import CursorSpotlightHeading from './components/CursorSpotlight/CursorSpotlightHeading';
 import CursorSpotlightText from './components/CursorSpotlight/CursorSpotlightText';
+import Logo from './Icons/Logo';
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
 
 export default function Contact() {
+  const [mounted, setMounted] = useState(false);
   const router = useRouter();
+  const { resolvedTheme } = useTheme();
+  const [logoTextColor, setLogoTextColor] = useState('');
+  const [logo, setLogo] = useState(<Logo color={logoTextColor} />);
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      resolvedTheme.includes('darkNeutral') && setLogoTextColor('#b3b3b3');
+      resolvedTheme.includes('light') && setLogoTextColor('#262626');
+      resolvedTheme.includes('darkSlate') && setLogoTextColor('#e1e7ef');
+      resolvedTheme.includes('darkGray') && setLogoTextColor('#e5e7eb');
+      resolvedTheme.includes('darkZinc') && setLogoTextColor('#e4e4e7');
+      resolvedTheme.includes('darkStone') && setLogoTextColor('#e7e5e4');
+    }
+    setMounted(true);
+  }, [resolvedTheme]);
+
+  useEffect(() => setLogo(<Logo color={logoTextColor} />), [logoTextColor]);
+
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
@@ -36,7 +59,8 @@ export default function Contact() {
   return (
     <div className='w-full h-full gap-8 px-4 py-12 lg:px-16 lg:py-16 flex flex-col justify-start items-center max-w-4xl'>
       <div className='flex flex-col justify-start items-center w-full gap-5 px-4 text-center'>
-        <CursorSpotlightHeading>Great Idea Development</CursorSpotlightHeading>
+        <div className='w-full px-8 max-w-lg logoColor'>{logo}</div>
+        <CursorSpotlightHeading>Drop us a line!</CursorSpotlightHeading>
         <CursorSpotlightText>
           Interested in partnering with us for your next project? We would love to hear from you!
         </CursorSpotlightText>
